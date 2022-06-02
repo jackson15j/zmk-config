84bc4a23342d677a3d800dfc3d7d1b79fa7c7253
Craig Astill <craig.astill@gmail.com>
2022-06-02 23:55:19 +0100

    Add emacs `compile-command` for Ferris board.

---
+++ config/.dir-locals.el
@@ -0,0 +1,4 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((fundamental-mode . ((compile-command . "cd ~/github_forks/zmk/; source .venv/bin/activate; cd app/; west build -p -d build/ferris --board ferris_rev02 -- -DZMK_CONFIG=$HOME/github_repos/zmk-config/config/ && west flash -d build/ferris"))))
