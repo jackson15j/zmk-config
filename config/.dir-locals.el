2ecc21b7a6b81dabd2428c44b20821dfc3fe4e5e
Craig Astill <craig.astill@gmail.com>
2022-06-03 14:22:40 +0100

    Use emacs file local variables for compile-command.
    Set cradio to build each half, instead of flashing at the end of the build.
    
    Issue: #4

--- config/.dir-locals.el
+++
@@ -1,5 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((c-mode . ((compile-command . "cd ~/github_forks/zmk/; source .venv/bin/activate; cd app/; west build -p -d build/ferris --board ferris_rev02 -- -DZMK_CONFIG=$HOME/github_repos/zmk-config/config/ && west flash -d build/ferris")))
- (fundamental-mode . ((compile-command . "cd ~/github_forks/zmk/; source .venv/bin/activate; cd app/; west build -p -d build/ferris --board ferris_rev02 -- -DZMK_CONFIG=$HOME/github_repos/zmk-config/config/ && west flash -d build/ferris"))))
