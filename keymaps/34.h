67b912e269faf1c687b5df39a98f52e3046e75b9
Craig Astill <craig.astill@gmail.com>
2023-04-29 00:24:08 +0100

    Blindly fix missing brackets.

--- keymaps/34.h
+++ keymaps/34.h
@@ -228,8 +228,9 @@
         &bt BT_CLR    &out OUT_BLE  &out OUT_USB  &reset        &bootloader       &trans      &kp PG_UP     &kp UP        &kp PG_DN  &kp DEL
         &bt BT_SEL 0  &bt BT_SEL 1  &bt BT_SEL 2  &bt BT_SEL 3  &bt BT_SEL 4      &kp HOME    &kp LEFT      &kp DOWN      &kp RIGHT  &kp END
         &to MAC_L  &to COLEMAK_L  &to COLEMAK_L   &to DEFAULT  &to GAME_L         &kp C_MUTE  &kp C_VOL_DN  &kp C_VOL_UP  &kp INS    &kp PSCRN
-                                                  &trans        &to DEFAULT       &to DEFAULT &trans
-  };
+                                                  &trans        &to DEFAULT       &to DEFAULT
+       >;
+   };
 
     function_layer {
       bindings = <
@@ -239,6 +240,5 @@
                                 &trans  &trans    &trans  &trans
         >;
     };
-
-
+  };
 };
